aa18a99b813d1bcfa60effc4ce3cf5ebfff0079e
minsoo <bellamy122@naver.com>
2023-08-09 18:39:52 +0900

    워크플로우 작업 진행 중

--- flaskProject2/db_conn.py
+++ flaskProject2/db_conn.py
@@ -8,15 +8,23 @@ def get_pool_conn():
     """
 
     """
-
-    config = {
-        'user': 'root'
-        , 'password': 'root'
-        , 'host': '127.0.0.1'
-        , 'database': 'EdgeCPS'
-              }
-    # Mariadb 커넥션 풀 설정
-    mariadb_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **config)
-
+    try:
+        config = {
+            'user': 'root'
+            , 'password': 'root'
+            , 'host': '127.0.0.1'
+            , 'database': 'EdgeCPS'
+                  }
+        # Mariadb 커넥션 풀 설정
+        mariadb_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **config)
+    except :
+        config = {
+            'user': 'minsoo'
+            , 'password': '1111'
+            , 'host': '127.0.0.1'
+            , 'database': 'EdgeCPS'
+        }
+        # Mariadb 커넥션 풀 설정
+        mariadb_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **config)
     return mariadb_pool
 
